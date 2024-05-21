1764e011f160ab1025d6e95325eb95fbaae102ab
Manish Thakur <manishhakur.in@hotmail.com>
2024-05-21 15:42:54 -0400

    Added initial react code

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Teampage } from './pages/TeamPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Teampage></Teampage>
+    </div>
+  );
+}
+
+export default App;
