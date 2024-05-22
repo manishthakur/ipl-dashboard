43f14bb2ecfa6e3a0d31b5bfc9994428c81420c6
Manish Thakur <manishhakur.in@hotmail.com>
2024-05-21 22:54:50 -0400

    feat: added link to team home pages

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,9 +1,13 @@
 import { React } from 'react'
+import { Link } from 'react-router-dom'
 
-export const MatchSmallCard = ({ match }) => {
+export const MatchSmallCard = ({ match, teamName }) => {
+    const otherTeam = match.team1 == teamName ? match.team2 : match.team1;
+    const otherTeamRoute = '/teams/' + otherTeam;
     return (
         <div className="MatchSmallCard">
-            <p>{match.team1} vs {match.team2}</p>
+            <p> vs <Link to={otherTeamRoute}> {otherTeam}</Link></p>
+            <h3>{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
         </div>
     );
 }
