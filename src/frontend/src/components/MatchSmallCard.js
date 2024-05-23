c63b0bb9a794b7bbc21e2b7501b7b1fc0b563195
Manish Thakur <manishhakur.in@hotmail.com>
2024-05-23 00:05:17 -0400

    Added styles to TeamPage

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,13 +1,19 @@
 import { React } from 'react'
 import { Link } from 'react-router-dom'
+import './MatchSmallCard.scss'
 
 export const MatchSmallCard = ({ match, teamName }) => {
-    const otherTeam = match.team1 == teamName ? match.team2 : match.team1;
+
+    const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
     const otherTeamRoute = '/teams/' + otherTeam;
+    const isMatchWon = teamName === match.matchWinner;
+
     return (
-        <div className="MatchSmallCard">
-            <p> vs <Link to={otherTeamRoute}> {otherTeam}</Link></p>
-            <h3>{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
+        
+        <div className={isMatchWon ? 'MatchSmallCard won-card' : 'MatchSmallCard lost-card'}>
+            <span className="vs">vs</span>
+            <h1><Link to={otherTeamRoute}> {otherTeam}</Link></h1>
+            <p className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</p>
         </div>
     );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,12 +1,14 @@
-import {React, useEffect, useState} from 'react'
-import {MatchDetailCard} from '../components/MatchDetailCard';
-import {MatchSmallCard} from '../components/MatchSmallCard';
-import {useParams} from 'react-router-dom';
+import './TeamPage.scss';
+import { React, useEffect, useState } from 'react'
+import { MatchDetailCard } from '../components/MatchDetailCard';
+import { MatchSmallCard } from '../components/MatchSmallCard';
+import { useParams } from 'react-router-dom';
+import { PieChart } from 'react-minimal-pie-chart';
 
 export const TeamPage = () => {
 
-    const [team, setTeam] = useState({matches: []});
-    const {teamName} = useParams();
+    const [team, setTeam] = useState({ matches: [] });
+    const { teamName } = useParams();
 
     useEffect(
         () => {
@@ -25,10 +27,27 @@ export const TeamPage = () => {
 
     return (
         <div className="TeamPage">
-            <h1>{team.teamName}</h1>
-            <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
-            {team.matches.slice(1).map(match => <MatchSmallCard match={match} teamName={teamName}/>)}
-        </div>
+            <div className="team-name-section">
+                <h1 className='team-name'>{team.teamName}</h1>
+            </div>
+            <div className="win-loss-section">
+                Wins / Losses
+                <PieChart          
+                        data={[
+                            { title: 'Wins', value: team.totalWins, color: '#4da375' },
+                            { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#a34d5d' },
+                        ]}
+                   
+                />
+            </div>
+            <div className="match-detail-section">
+                <h3>Latest Matches</h3>
+                <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+            </div>
+
+            { team.matches.slice(1).map(match => <MatchSmallCard match={match} teamName={teamName} />) }
+    <div className='more-link'><a href='#'>More</a></div>
+        </div >
     );
-    
+
 }
